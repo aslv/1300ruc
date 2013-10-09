2100d64fba1d08b231dc70af667302e074da3db3
aslv <aslv@abv.bg>
2013-10-09 20:45:28 +0300

    added homework and demos

---
+++ lection1/demos/FloatValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    class FloatValues
+    {
+        static void Main(string[] args)
+        {
+            double a = 0.1f;
+            Console.WriteLine("Error in doubles? {0:N25}",10*a);
+
+            float b = 0.1f;
+            Console.WriteLine("Error in floats? {0:N25}",b+b+b+b+b);
+        }
+    }
+}
---
+++ lection1/demos/IntegerValues.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    class IntegerValues
+    {
+        static void Main(string[] args)
+        {
+            //Signed byte values
+            Console.WriteLine("signed byte values");
+            sbyte sbyteA = SByte.MaxValue;
+            Console.WriteLine(sbyteA);
+            sbyteA = SByte.MinValue;
+            Console.WriteLine(sbyteA);
+            //unsigned byte values
+            Console.WriteLine("unsigned byte values");
+            byte byteA = Byte.MaxValue;
+            Console.WriteLine(byteA);
+            byteA = Byte.MinValue;
+            Console.WriteLine(byteA);
+            //Short values
+            Console.WriteLine("short values");
+            short shortA = short.MaxValue;
+            Console.WriteLine(shortA);
+            shortA = short.MinValue;
+            Console.WriteLine(shortA);
+            //int values
+            Console.WriteLine("int values");
+            int intA = Int32.MaxValue;
+            Console.WriteLine(intA);
+            intA = Int32.MinValue;
+            Console.WriteLine(intA);
+            //long values
+            Console.WriteLine("long values");
+            long longA = long.MaxValue;
+            Console.WriteLine(longA);
+            longA = long.MinValue;
+            Console.WriteLine(longA);
+        }
+    }
+}
